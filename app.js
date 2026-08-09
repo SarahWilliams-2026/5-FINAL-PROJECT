2d8b74282cd826d45ab31494fafb0c3e2a0612e0
Sarahhassweetcheeks <Sarahhassweetcheeks@gmail.com>
2026-08-09 12:09:33 -0400

    error in JS with movieTitleData

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=97a8a533&s=${title}
 
 async function main() {
-    const movieTitle = await fetch(`https://omdbapi.com/`);
+    const movieTitle = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=97a8a533&s=${"twilight"}`);
     const movieTitleData = await movieTitle.json();
+    const movieListEl = document.querySelector(".movie-list");
     console.log(movieTitleData);
+    
+    movieListEl.innerHTML = movieTitleData
+        .map(
+            (movieTitle) => `<div class="movie-card">
+                <div class="movie-card__container">
+                    <h3>Movie Title</h3>
+                        <p><b>Type:</b> movie</p>
+                        <p><b>Year:</b> 0000</p>
+                    <p><b>Cover Art:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+                </div>
+            </div>` 
+        )
+        .join("");
 }
 
 main();
